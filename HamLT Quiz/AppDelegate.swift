63e64662c3f8e260cc397b8f096564414d038ab2
kesrut <planasb@planasbs-imac.dlink>
2018-02-21 20:21:17 +0200

    Initial Commit

---
+++ HamLT Quiz/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HamLT Quiz
+//
+//  Created by planasb on 21/02/2018.
+//  Copyright © 2018 Kęstutis Rutkauskas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
